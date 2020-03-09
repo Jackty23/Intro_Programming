42411c07bb4c1e129471dd6c97e7d895975e4892
LTaylor137 <103200214@student.swin.edu.au>
2020-03-09 22:28:07 +1100

    moved everything into sort me folder

---
+++ sortme/Guessthenumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Guessthenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guess1;
+
+            {
+                Console.WriteLine("Hey there! Let's play a little guessing game.");
+Console.WriteLine("Guess the number between 0 and 10");
+                guess1 = Console.ReadLine ();
+            }
+        }
+    }
+}

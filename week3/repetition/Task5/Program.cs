d59a29d0973cab8ba5877f48a502c357780d6acf
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-03-19 14:04:01 +1100

    wk3 task 4 and 5

---
+++ week3/repetition/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What happens when you throw a yellow rock into the red sea?");
+
+string correctanswer = "it gets wet";
+string guess = "noguess";
+
+while(correctanswer != guess){
+
+Console.WriteLine("take a guess...");
+guess = (Console.ReadLine ());
+
+}
+
+Console.WriteLine("You've got it.");
+
+        }
+    }
+}
---
+++ week3/repetition/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace repetition {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("enter a number, and i'll do the counting for you");
+
+            int counter = 0;
+            int input;
+
+            input = int.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("start count");
+
+            while (counter <= input) {
+
+                Console.WriteLine ($"{counter}");
+                counter = counter+1;
+
+            }
+
+            Console.WriteLine ("counting complete");
+
+        }
+    }
+}

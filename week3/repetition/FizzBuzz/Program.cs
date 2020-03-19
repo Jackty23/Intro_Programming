3d24c289b495a73b84d0db2f76e1cef47e0f089a
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-03-19 15:58:38 +1100

    fizzbuzz complete

---
+++ week3/repetition/FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz {
+    class Program {
+        static void Main (string[] args) {
+
+            int counter = 1;
+
+            while (counter <= 100) {
+
+                if (counter % 3 == 0) {
+                    // AND
+                    if (counter % 5 == 0) {
+                        Console.WriteLine ("FizzBuzz");
+                    } else {
+                        Console.WriteLine ("Fizz");
+                    }
+
+                } else {
+
+                    if (counter % 5 == 0) {
+                        Console.WriteLine ("Buzz");
+
+                    } else {
+                        Console.WriteLine (counter);
+                    }
+
+                }
+                counter = counter + 1;
+            }
+        }
+    }
+}

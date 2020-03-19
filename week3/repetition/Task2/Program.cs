a056f8bb4141ab43f93f8f12ab94bbbf60783ffa
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-03-19 13:47:36 +1100

    wk3 task 2 and 3

---
+++ week3/repetition/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start counting!");
+
+
+int counter = 2;
+
+while(counter <=20){
+
+Console.WriteLine($"counter = {counter}");
+counter = counter +2;
+
+}
+
+Console.WriteLine("end counting!");
+
+        }
+    }
+}
---
+++ week3/repetition/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the magic number, it's 5!");
+
+int magicnumber = 5;
+int guess = 0;
+
+while(guess != magicnumber){
+
+Console.WriteLine("take a guess...");
+guess = int.Parse (Console.ReadLine ());
+
+}
+
+Console.WriteLine("You've got it.");
+
+        }
+    }
+}

cf2abcd5467168515aa3ecbadf54670c6deb90ef
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-03-30 23:16:17 +1100

    wk4 nestreptask2

---
+++ week4/nestedrepetitiontask2/nestedrepetitiontask2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace nestedrepetitiontask2 {
+    class Program {
+        static void Main (string[] args) {
+
+            string x = "x";
+            int counter = 0;
+
+            while (counter <= 5) {
+
+                counter++;
+
+                int counter2 = counter;
+
+                while (counter2 <= 5) {
+
+                    System.Console.Write (x);
+
+                    counter2++;
+
+                }
+
+                System.Console.WriteLine ();
+
+            }
+
+            // ----------------------------
+
+            counter = 0;
+
+            while (counter <= 5) {
+
+                counter++;
+
+                int counter2 = counter;
+
+                while (counter2 > 1) {
+
+                    System.Console.Write (x);
+                    counter2--;
+
+                }
+
+                System.Console.WriteLine ();
+
+            }
+        }
+    }
+}
